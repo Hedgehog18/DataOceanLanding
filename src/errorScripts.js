013e18aa1584ac3d79d08f6745f61f3aab3964aa
AlenaYanish <yanishevskayaar@gmail.com>
2021-04-06 14:59:26 +0300

    add 404 page to landing DO

---
+++ src/errorScripts.js
@@ -0,0 +1,20 @@
+import './scss/main.scss';
+import { allowedLanguages, t, changeLang } from './localization';
+// import { allowedLanguages, t, changeLang } from './localization';
+
+$('.link-platform').on('click', function () {
+    location.replace(`${process.env.DO_FRONTEND_HOST}/system/home/?lang=${localStorage.getItem('lang')}`);
+});
+
+$('.link-cpk').on('click', function () {
+    location.replace(`https://pep.org.ua/${localStorage.getItem('lang')}`);
+});
+
+$('#change-lang').on('click', function(event) {
+    event.preventDefault();
+    let langUser = 'uk';
+    if (localStorage.getItem('lang') === 'uk') {
+        langUser = 'en';
+    }
+    changeLang(langUser);
+});
